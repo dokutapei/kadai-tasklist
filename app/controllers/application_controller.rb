53b388e9fcf5ea5c18704801d64f9a0bf4558267
dokuta.pei <dokupe1111@gmail.com>
2020-05-22 14:20:13 +0000

    tasklist2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,12 @@
 class ApplicationController < ActionController::Base
 
   include SessionsHelper
-
+  
   private
 
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
-  end
-
-  def counts(user)
-    @count_taskss = user.tasks.count
   end
 end
