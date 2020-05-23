9467da4c801a489edc88f889c43b41aaf1098044
dokuta.pei <dokupe1111@gmail.com>
2020-05-23 08:51:33 +0000

    tasklist4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = current_user.tasks
   end
   
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -17,23 +17,21 @@ class TasksController < ApplicationController
     @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクを投稿しました。'
-      redirect_to root_url
+      redirect_to @task
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクの投稿に失敗しました。'
-      render 'tasks/index'
+      render :new
     end
   end
   
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
-      redirect_to task_url
+      redirect_to @task
     else
       flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
@@ -41,15 +39,21 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = current_user.tasks.find_by(id: params[:id])
     @task.destroy
     flash[:success] = 'タスクを削除しました。'
-    redirect_to tasks_path
+    redirect_back(fallback_location: tasks_path)
   end
 
   private
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_path
+    end
   end
 end
